e67b8d8731a68994c1b32fa64b7df7d4e4a3b93f
Geovanny Afanador <Geovany.afanador@gmail.com>
2021-02-01 23:24:58 -0500

    commit de query methods
    Se realizaron las consultas con query methods

--- spring-market/src/main/java/com/spring/market/persistence/ProductoRepository.java
+++ spring-market/src/main/java/com/spring/market/persistence/ProductoRepository.java
@@ -4,13 +4,19 @@ import com.spring.market.persistence.crud.ProductoCrudRepository;
 import com.spring.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
 
-
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
