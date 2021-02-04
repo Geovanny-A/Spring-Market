be6cea6228b52791b344f220baaaef8f328c631c
Geovanny Afanador <Geovany.afanador@gmail.com>
2021-02-03 23:34:21 -0500

    Update ProductoRepository.java

--- spring-market/src/main/java/com/spring/market/persistence/ProductoRepository.java
+++ spring-market/src/main/java/com/spring/market/persistence/ProductoRepository.java
@@ -2,23 +2,39 @@ package com.spring.market.persistence;
 
 import com.spring.market.persistence.crud.ProductoCrudRepository;
 import com.spring.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
     public List<Producto> getByCategoria(int idCategoria){
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
-
     }
+
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
     public Optional<List<Producto>> getProductoByPrecioVentaLessThanPriceAndByCategory (int precioVenta, int idCategoria){
         return productoCrudRepository.findByPrecioVentaLessThanAndIdCategoriaOrderByNombreAsc(precioVenta, idCategoria);
     }
