7c12eb04b8e9089a887483649ecd55ee18520d56
Geovanny Afanador <Geovany.afanador@gmail.com>
2021-02-04 19:36:40 -0500

    commit de mapstruct

---
+++ spring-market/src/main/java/com/spring/market/domain/Product.java
@@ -0,0 +1,67 @@
+package com.spring.market.domain;
+
+public class Product {
+    private int productId;
+    private String name;
+    private int categoriyId;
+    private double price;
+    private int stock;
+    private boolean active;
+    private Category category;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategoriyId() {
+        return categoriyId;
+    }
+
+    public void setCategoriyId(int categoriyId) {
+        this.categoriyId = categoriyId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
