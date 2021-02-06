533a1f4c8feaea64216332c296468c96a5a23b46
Geovanny Afanador <Geovany.afanador@gmail.com>
2021-02-06 01:08:32 -0500

    commit orientar la api
    se oriento la api al dominio con mapstruct

---
+++ spring-market/src/main/java/com/spring/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,31 @@
+package com.spring.market.persistence.mapper;
+
+
+import com.spring.market.domain.Product;
+import com.spring.market.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "Spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoriyId")
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category")
+    })
+    Product toProduct(Producto producto);
+
+    List<Product> toProducts(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+}
