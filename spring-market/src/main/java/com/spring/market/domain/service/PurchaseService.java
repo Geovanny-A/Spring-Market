44b6ec9e44dd7a7c12bcf11673a702104e1c93ec
Geovanny Afanador <Geovany.afanador@gmail.com>
2021-02-12 21:30:28 -0500

    commit de api para compras
    se configuro el api para las compras

---
+++ spring-market/src/main/java/com/spring/market/domain/service/PurchaseService.java
@@ -0,0 +1,27 @@
+package com.spring.market.domain.service;
+
+import com.spring.market.domain.Purchase;
+import com.spring.market.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll(){
+        return purchaseRepository.getAll();
+    }
+    public Optional<List<Purchase>> getByClient(String clientId){
+        return purchaseRepository.getByClient(clientId);
+    }
+    public Purchase save(Purchase purchase){
+        return purchaseRepository.save(purchase);
+    }
+
+}
